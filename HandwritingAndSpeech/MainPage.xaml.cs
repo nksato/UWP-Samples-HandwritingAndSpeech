c9e26cc67fb2ddbd19ad478db5018fd5767a19c4
Naoki Sato <naokis@live.com>
2015-11-27 17:31:08 +0900

    1st release

---
+++ HandwritingAndSpeech/MainPage.xaml.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+using Windows.UI.Input.Inking;
+using Windows.Globalization;
+using Windows.Media.SpeechSynthesis;
+using Windows.Media.SpeechRecognition;
+using System.Threading.Tasks;
+using Windows.UI.Core;
+using SpeechAndTTS;
+
+// 空白ページのアイテム テンプレートについては、http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409 を参照してください
+
+namespace Handwriting
+{
+    /// <summary>
+    /// それ自体で使用できる空白ページまたはフレーム内に移動できる空白ページ。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        InkRecognizerContainer inkRecognizerContainer = null;
+        private IReadOnlyList<InkRecognizer> recoView = null;
+
+        private SpeechSynthesizer synthesizer;
+        private IAsyncOperation<SpeechRecognitionResult> recognitionOperation;
+        private SpeechRecognizer speechRecognizer;
+
+        private CoreDispatcher dispatcher;
+
+        private static uint HResultPrivacyStatementDeclined = 0x80045509;
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            InkDrawingAttributes drawingAttributes = new InkDrawingAttributes();
+            drawingAttributes.Color = Windows.UI.Colors.Black;
+            drawingAttributes.Size = new Size(4, 4);
+            drawingAttributes.IgnorePressure = false;
+            drawingAttributes.FitToCurve = true;
+
+
+            inkRecognizerContainer = new InkRecognizerContainer();
+            recoView = inkRecognizerContainer.GetRecognizers();
+            if (recoView.Count > 0)
+            {
+                foreach (InkRecognizer recognizer in recoView)
+                {
+                    RecoName.Items.Add(recognizer.Name);
+                }
+            }
+            else
+            {
+                RecoName.IsEnabled = false;
+                RecoName.Items.Add("No Recognizer Available");
+            }
+            RecoName.SelectedIndex = 0;
+
+
+
+            InkCanvas.InkPresenter.UpdateDefaultDrawingAttributes(drawingAttributes);
+            InkCanvas.InkPresenter.InputDeviceTypes = 
+                Windows.UI.Core.CoreInputDeviceTypes.Mouse |
+                Windows.UI.Core.CoreInputDeviceTypes.Pen |
+                Windows.UI.Core.CoreInputDeviceTypes.Touch;
+
+            // 音声合成の追加
+            synthesizer = new SpeechSynthesizer();
+
+        }
+
+        void OnRecognizerChanged(object sender, RoutedEventArgs e)
+        {
+            string selectedValue = (string)RecoName.SelectedValue;
+            SetRecognizerByName(selectedValue);
+        }
+
+        bool SetRecognizerByName(string recognizerName)
+        {
+            bool recognizerFound = false;
+
+            foreach (InkRecognizer reco in recoView)
+            {
+                if (recognizerName == reco.Name)
+                {
+                    inkRecognizerContainer.SetDefaultRecognizer(reco);
+                    recognizerFound = true;
+                    break;
+                }
+            }
+
+            if (!recognizerFound)
+            {
+                Status.Text = "Could not find target recognizer.";
+            }
+
+            return recognizerFound;
+        }
+
+        void OnClear(object sender, RoutedEventArgs e)
+        {
+            InkCanvas.InkPresenter.StrokeContainer.Clear();
+        }
+
+        async void OnRecognizeAsync(object sender, RoutedEventArgs e)
+        {
+            IReadOnlyList<InkStroke> currentStrokes = InkCanvas.InkPresenter.StrokeContainer.GetStrokes();
+            if (currentStrokes.Count > 0)
+            {
+                RecognizeBtn.IsEnabled = false;
+                ClearBtn.IsEnabled = false;
+                RecoName.IsEnabled = false;
+
+                var recognitionResults = await inkRecognizerContainer.RecognizeAsync(InkCanvas.InkPresenter.StrokeContainer, InkRecognitionTarget.All);
+
+                if (recognitionResults.Count > 0)
+                {
+                    // Display recognition result
+                    string str = ""; //  "Recognition result:";
+                    foreach (var r in recognitionResults)
+                    {
+                        str += " " + r.GetTextCandidates()[0];
+                    }
+                    Status.Text = str;
+                }
+                else
+                {
+                    Status.Text = "No text recognized.";
+                }
+
+                RecognizeBtn.IsEnabled = true;
+                ClearBtn.IsEnabled = true;
+                RecoName.IsEnabled = true;
+            }
+            else
+            {
+                Status.Text = "Must first write something.";
+            }
+        }
+
+
+        async void OnSpeechAsync(object sender, RoutedEventArgs e)
+        {
+            if (media.CurrentState.Equals(MediaElementState.Playing))
+            {
+                media.Stop();
+            }
+            else
+            {
+                string text = Status.Text;
+                if (!String.IsNullOrEmpty(text))
+                {
+  
+                    try
+                    {
+                        // 話す言葉（テキスト）をセットし 
+                        SpeechSynthesisStream synthesisStream = await synthesizer.SynthesizeTextToStreamAsync(text);
+
+                        // メディアで話す
+                        media.AutoPlay = true;
+                        media.SetSource(synthesisStream, synthesisStream.ContentType);
+                        media.Play();
+                    }
+                    catch (Exception)
+                    {
+                        // If the text is unable to be synthesized, throw an error message to the user.
+                        var messageDialog = new Windows.UI.Popups.MessageDialog("Unable to synthesize text");
+                        await messageDialog.ShowAsync();
+                    }
+                }
+            }
+        }
+
+        private async void Recognize_Click(object sender, RoutedEventArgs e)
+        {
+   
+            // Start recognition.
+            try
+            {
+
+                // 音声認識を行っている部分
+                recognitionOperation = speechRecognizer.RecognizeWithUIAsync();
+                //recognitionOperation = speechRecognizer.RecognizeAsync();
+                SpeechRecognitionResult speechRecognitionResult = await recognitionOperation;
+
+                if (speechRecognitionResult.Status == SpeechRecognitionResultStatus.Success)
+                {
+                    Status.Text = speechRecognitionResult.Text;
+                }
+                else
+                {
+                    Status.Text = string.Format("Speech Recognition Failed, Status: {0}", speechRecognitionResult.Status.ToString());
+                }
+            }
+            catch (TaskCanceledException exception)
+            {
+                // TaskCanceledException will be thrown if you exit the scenario while the recognizer is actively
+                // processing speech. Since this happens here when we navigate out of the scenario, don't try to 
+                // show a message dialog for this exception.
+                System.Diagnostics.Debug.WriteLine("TaskCanceledException caught while recognition in progress (can be ignored):");
+                System.Diagnostics.Debug.WriteLine(exception.ToString());
+            }
+            catch (Exception exception)
+            {
+                // Handle the speech privacy policy error.
+                if ((uint)exception.HResult == HResultPrivacyStatementDeclined)
+                {
+                }
+                else
+                {
+                    var messageDialog = new Windows.UI.Popups.MessageDialog(exception.Message, "Exception");
+                    await messageDialog.ShowAsync();
+                }
+            }
+        }
+
+        protected async override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // Save the UI thread dispatcher to allow speech status messages to be shown on the UI.
+            dispatcher = CoreWindow.GetForCurrentThread().Dispatcher;
+
+            bool permissionGained = await AudioCapturePermissions.RequestMicrophonePermission();
+            if (permissionGained)
+            {
+            }
+            else
+            {
+            }
+            
+            await InitializeRecognizer(SpeechRecognizer.SystemSpeechLanguage);
+        }
+
+        private async Task InitializeRecognizer(Language recognizerLanguage)
+        {
+            if (speechRecognizer != null)
+            {
+                this.speechRecognizer.Dispose();
+                this.speechRecognizer = null;
+            }
+
+            speechRecognizer = new SpeechRecognizer(recognizerLanguage);
+
+            var dictationConstraint = new SpeechRecognitionTopicConstraint(SpeechRecognitionScenario.Dictation, "dictation");
+            speechRecognizer.Constraints.Add(dictationConstraint);
+            SpeechRecognitionCompilationResult compilationResult = await speechRecognizer.CompileConstraintsAsync();
+
+
+            if (compilationResult.Status != SpeechRecognitionResultStatus.Success)
+            {
+                Status.Text = "エラー";
+            }
+        }
+
+
+    }
+}
